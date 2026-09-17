1ab9be410e8561716ae7e4f23df20f5e27fbb757
raizuwi04 <raizuwi04@gmail.com>
2026-09-18 02:21:15 +0800

    localhost 5500

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/";
+const API_URL = "http://localhost:5500/";
 
 const loginForm = document.getElementById("loginForm");
 const registerForm = document.getElementById("registerForm");
