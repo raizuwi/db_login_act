d9729b5c663be379ba8d384500a602dbea8f6ae4
raizuwi04 <raizuwi04@gmail.com>
2026-09-18 02:45:42 +0800

    proper port

--- backend/server.js
+++ backend/server.js
@@ -112,12 +112,12 @@ app.get("/api/profile", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("MongoDB connected.");
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
